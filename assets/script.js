0972a8e46d9cb273833cbd50f5c453baf43b0afd
Tyler Smith <mrtbird539@gmail.com>
2022-01-24 20:30:36 -0500

    fixed logic and added toast if no value entered

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,41 @@
-
-const select$ = $('select');
-
+const rocketBtn$ = $(".rocket-btn")
+const select$ = $("select");
+const toast$ = $('.toast');
+//Function for displaying all rockets in a dropdown and attaching the Id
+//of the rocket for later queries
 function renderRocketOptions() {
     const url = "https://api.spacexdata.com/v4/rockets";
 
     fetch(url)
-        .then(function(response) {
-            console.log(response)
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log(data)
             for (var i = 0; i < data.length; i++) {
                 let rocketName = data[i].name;
+                let rocketId = data[i].id;
                 let rocketOptions = document.createElement("option");
-                rocketOptions.innerHTML = rocketName
+                rocketOptions.innerHTML = rocketName;
+                rocketOptions.value = rocketId;
                 select$.append(rocketOptions);
             }
             return
-            });
+        });
 }
 
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const inputVal = $("select option:selected").val();
+    //Quick verification of input
+    if (!inputVal) {
+        return toast$.toast("show");
+    }
+    else {
+    location.assign(`./rocket.html?q=${inputVal}`)
+    };
+
+}
+
+rocketBtn$.click(handleFormSubmit);
 renderRocketOptions();
