630b40f5e2b9324220d08dbde62b71cc9937d7e2
Tyler Smith <mrtbird539@gmail.com>
2022-01-24 23:09:48 -0500

    fixed cors issue

--- assets/rocket_script.js
+++ assets/rocket_script.js
@@ -6,25 +6,42 @@ function getRocketId() {
 
 
 function displayRocketData(rocketId) {
-    const fetchBody = `{
-        "query": {
-        "rocket": "${rocketId}"
+
+    const fetchBody = {
+        query: {
+            rocket: rocketId
+        },
+        options: {
+            sort: {
+                date_unix: "desc"
+            }
         }
-    }`
-    const url = "http://api.spacexdata.com/v4/launches/query"
+    }
+    const url = "https://api.spacexdata.com/v4/launches/query"
 
     fetch(url, {
         method: "POST",
         mode: "cors",
         credentials: "same-origin",
         body: JSON.stringify(fetchBody),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
     })
-    .then(function(response) {
-    return response.json()
-    })
-    .then(function(data) {
-        console.log(data);
-    })
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.docs.length; i++) {
+                console.log(data.docs[i].date_unix)
+                console.log(data.docs[i].name)
+                console.log(data.docs[i].links.patch.small)
+                console.log(data.docs[i].success)
+                console.log(data.docs[i].details)
+                console.log(data.docs[i].failures[0].reason)
+            }
+        })
 }
 
 
--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,6 @@ function renderRocketOptions() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
             for (var i = 0; i < data.length; i++) {
                 let rocketName = data[i].name;
                 let rocketId = data[i].id;
@@ -19,6 +18,7 @@ function renderRocketOptions() {
                 rocketOptions.innerHTML = rocketName;
                 rocketOptions.value = rocketId;
                 select$.append(rocketOptions);
+                console.log(rocketId)
             }
             return
         });
